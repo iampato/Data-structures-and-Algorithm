0596dd8fdf5cdefd81bcb2127899a67674ddfadf
iampato <858wpwaweru@gmail.com>
2022-09-28 22:43:31 +0300

    Finished on LinkedList

--- LinkedList/src/me/iampato/datastructures/linkedlist/LinkedList.java
+++ LinkedList/src/me/iampato/datastructures/linkedlist/LinkedList.java
@@ -1,26 +1,139 @@
 package me.iampato.datastructures.linkedlist;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 public class LinkedList {
-    private class Node{
+    private class Node {
         private String value;
         private Node nextNode;
 
-        public Node(String v){
+        public Node(String v) {
             value = v;
         }
     }
+
     private Node first;
     private Node last;
 
     // addFirst
-    public void addFirst(String value){
+    public void addFirst(String value) {
         var node = new Node(value);
-        // 
+        // check if the first node is empty
+        if (first == null) {
+            first = last = node;
+        } else {
+            // 10 -> 20
+            // we want to add 5, we need to link 5 to the current 1st node then
+            // assign a new first node
+            // 5(node) -> 10(first) -> 20
+            node.nextNode = first;
+            first = node;
+        }
     }
+
     // addLast
-    // removeFirst
-    // removeLast
+    public void addLast(String value) {
+        var node = new Node(value);
+        // check if list is empty
+        if (first == null) {
+            first = last = node;
+        } else {
+            // 10 -> 20
+            // we want to add 30, we need to link 30 to the last node(20) then
+            // assign a new last node(30)
+            // 10 -> 20(current last) -> 30(new last)
+            last.nextNode = node;
+            last = node;
+        }
+    }
+
     // size
+    public int size() {
+        int length = 0;
+        var current = first;
+        while (current != null) {
+            length++;
+            current = current.nextNode;
+        }
+        return length;
+    }
+
+    // to array
+    public String[] toArray() {
+        String[] array = new String[size()];
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            array[index] = current.value;
+            index++;
+            current = current.nextNode;
+        }
+        return array;
+    }
+
+    // removeFirst
+    public void removeFirst() {
+        // "A" -> "B" -> "C"
+        // check first node is empty
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        // "B" needs to be the first element
+        // So get B
+        var secondNode = first.nextNode;
+        first = secondNode;
+    }
+
+    // removeLast
+    public void removeLast() {
+        // "A" -> "B" -> "C" -> "D"
+        // check first node is empty
+        if (last == null) {
+            throw new NoSuchElementException();
+        }
+        // we need to find "C" and we know "D"
+        // the relationship is the nextNode for C is equal to D
+        // iterate
+
+
+        // now we have "C" in secondLastNode
+        // we need to set secondLastNode nextNode to null
+        // then set last node to secondLastNode
+        var secondLastNode = getPreviousNode(last);
+        secondLastNode.nextNode = null;
+        last = secondLastNode;
+    }
+
+    private Node getPreviousNode(Node e) {
+        var current = first;
+        while (current != null) {
+            if (current.nextNode == e) {
+                break;
+            } else {
+                current = current.nextNode;
+            }
+        }
+        return current;
+    }
+
     // indexOf
+    public int indexOf(String item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item) {
+                return index;
+            } else {
+                index++;
+                current = current.nextNode;
+            }
+        }
+        return -1;
+    }
+
     // contains
+    public boolean contains(String item) {
+        return indexOf(item) != -1;
+    }
 }
--- LinkedList/src/me/iampato/datastructures/linkedlist/Main.java
+++ LinkedList/src/me/iampato/datastructures/linkedlist/Main.java
@@ -1,4 +1,27 @@
 package me.iampato.datastructures.linkedlist;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        var linkedList = new LinkedList();
+        linkedList.addLast("B");
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.addLast("C");
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.addLast("D");
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.addLast("E");
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.addFirst("A");
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.removeFirst();
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        linkedList.removeLast();
+        System.out.println(Arrays.toString(linkedList.toArray()));
+        //
+        System.out.println(linkedList.indexOf("C"));
+        System.out.println(linkedList.contains("D"));
+        System.out.println(linkedList.contains("A"));
+    }
 }
